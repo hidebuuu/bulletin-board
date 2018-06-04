64e66c976353310588cd75cc8074f165c8005545
ユーザ名 <hidebu09200202@gmail.com>
2018-06-04 11:23:09 +0900

    ジェネレーターの設定を修正しました

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module BoardApp
     config.time_zone = 'Tokyo'
     config.i18n.default_locale = :ja
     config.active_record.default_timezone = :local
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.test false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
