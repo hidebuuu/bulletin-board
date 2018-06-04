04a30287c09f85bc820bee0425775fde336d23bb
ユーザ名 <hidebu09200202@gmail.com>
2018-06-04 11:44:42 +0900

    endを付け忘れてました、、

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module BoardApp
       g.assets false
       g.helper false
       g.test false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
