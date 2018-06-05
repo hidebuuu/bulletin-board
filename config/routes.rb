31c9e484d421387a1efab235d41705b0d35023bc
ユーザ名 <hidebu09200202@gmail.com>
2018-06-05 14:30:01 +0900

    ルーティングの簡略化

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'root' => 'dashboards#index'
+  root 'dashboards#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
