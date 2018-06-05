6f5336256aab434f48fe5ecc8b3baecfcaa22401
ユーザ名 <hidebu09200202@gmail.com>
2018-06-05 13:02:23 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-get 'dashboards/index' => 'dashboards#index'
+get 'root' => 'dashboards#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
