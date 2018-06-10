5716877d5d9dd2d3343b0f8db528f8e9dbd93cb1
ユーザ名 <hidebu09200202@gmail.com>
2018-06-10 14:40:34 +0900

    余白・余分なルーティングを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #get 'users/new' => 'users#new'
-  #post 'users/create' => 'users#create'
-  resources :users, only: [:new, :create]
+  resources :users, only: %i[new create]
   get 'login' => 'user_sessions#new'
   post 'login' => 'user_sessions#create'
   delete 'logout' => 'user_sessions#destroy'
