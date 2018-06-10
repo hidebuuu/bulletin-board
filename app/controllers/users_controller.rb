980aa09ba611b9865a3f07d64f06e7302978a7a9
ユーザ名 <hidebu09200202@gmail.com>
2018-06-10 14:07:41 +0900

    コンマ後の余白追加・シンボルの書き方変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,22 @@
 class UsersController < GeneralController
-  skip_before_action :require_login, only: [:new,:create]
-  
-
+  skip_before_action :require_login, only: %i[new create]
 
   def new
-      binding.pry
     @user = User.new
-    render :layout => 'before_login'
+    render layout: 'before_login'
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-    redirect_to("/login")
+      redirect_to('/login')
     else
-      render("/users/new")
+      render('/users/new')
+    end
   end
-end
 
 private
-
   def user_params
-      params.require(:user).permit(:last_name, :first_name , :email ,:password,:password_confirmation)
-    end
+    params.require(:user).permit(:last_name, :first_name , :email ,:password,:password_confirmation)
+  end
 end
