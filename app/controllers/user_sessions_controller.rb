a14b689e4052b0544675b926f0e882ceb7ef812a
ユーザ名 <hidebu09200202@gmail.com>
2018-06-11 14:52:09 +0900

    クラスとアクションの間に行挿入

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,11 +1,12 @@
 class UserSessionsController < GeneralController
   skip_before_action :require_login, only: %i[new create destroy]
+  
   def new
     render layout: 'before_login'
   end
 
   def create
-    @user = login(params[:email], params[:password])
+    @user = login(params[:email],params[:password])
     if @user.present?
       redirect_to root_path
     else
