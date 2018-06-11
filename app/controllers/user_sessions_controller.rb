091ba07824c6c0e538a57845a5e66c7d1d76c18a
ユーザ名 <hidebu09200202@gmail.com>
2018-06-11 17:39:33 +0900

    flash messageの追加、それに伴いいくつかをrenderに修正

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -8,14 +8,17 @@ class UserSessionsController < GeneralController
   def create
     @user = login(params[:email], params[:password])
     if @user.present?
-      redirect_to root_path
+      flash[:success] = "ログインしました！"
+      redirect_to root_path 
     else
-      redirect_to login_path
+      flash.now[:danger] = "ログインに失敗しました"
+      render :new
     end
   end
 
   def destroy
     logout
-    redirect_to login_path
+    flash.now[:info] = "ログアウトしました。"
+    render :new
   end
 end
