569b4bfa42846caae717b66d38718c6966dc4961
ユーザ名 <hidebu09200202@gmail.com>
2018-06-13 14:27:27 +0900

    アソシエーションオプション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
-  has_many :boards
+  has_many :boards , dependent: :destroy
   validates :email, presence: true
   validates :password, presence: true
   validates :first_name, presence: true
