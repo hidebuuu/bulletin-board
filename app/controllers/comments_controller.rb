e6eb2cb35ab47d193c72f45fe732186a53ecd598
ユーザ名 <hidebu09200202@gmail.com>
2018-06-15 14:36:48 +0900

    commentsモデル・コントローラ・パーシャルビュー作成

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,16 @@
+class CommentsController < GeneralController
+
+  def create
+    @board=  Board.find_by(id: params[:board_id])
+    @comments = @board.comments.build(comment_params)
+    if @comments.save
+      redirect_to board_path(id: params[:board_id])
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, board_id: params[:board_id] )
+  end
+end
