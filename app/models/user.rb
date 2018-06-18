17deebf3413df85259578d0d9e3a1999cd75ffd3
ユーザ名 <hidebu09200202@gmail.com>
2018-06-18 18:00:47 +0900

    インデント・余白調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   def my_board?(board_user)
-    id  == board_user
+    id == board_user
   end
 end
