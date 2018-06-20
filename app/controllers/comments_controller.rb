fe528f057a2b4b287700ddf178af8700b711cc1a
ユーザ名 <hidebu09200202@gmail.com>
2018-06-20 18:25:53 +0900

    save出来なかった時の処理

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,9 @@ class CommentsController < GeneralController
     @board = Board.find_by(id: params[:board_id])
     @comment = @board.comments.new(comment_params)
     if @comment.save
-      redirect_to board_path(@comment.board)
+      redirect_to board_path(@comment.board), success: 'コメントを投稿しました。'
+    else
+      redirect_to board_path(@comment.board), danger: '空ではコメントできません。'
     end
   end
 
