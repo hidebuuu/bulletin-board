841c8a5dd3389d1b78ef91191329551b939371bd
ユーザ名 <hidebu09200202@gmail.com>
2018-06-20 16:03:27 +0900

    余白・インデント・末尾空白行修正

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -3,13 +3,13 @@ class FollowsController < ApplicationController
   def create
     @follow = Follow.new(user_id: current_user.id, board_id: params[:board_id])
     if @follow.save
-    redirect_to :back , success: 'お気に入りに登録しました。'
+      redirect_to :back, success: 'お気に入りに登録しました。'
     end
   end
 
   def destroy
     @follow = Follow.find(params[:id])
     @follow.destroy
-    redirect_to :back , danger: 'お気に入りを解除しました。'
+    redirect_to :back, danger: 'お気に入りを解除しました。'
   end
 end
--- app/decorators/follow_decorator.rb
+++ app/decorators/follow_decorator.rb
@@ -9,5 +9,4 @@ class FollowDecorator < ApplicationDecorator
   #       object.created_at.strftime("%a %m/%d/%y")
   #     end
   #   end
-
 end
