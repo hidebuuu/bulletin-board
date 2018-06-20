5ae453ef6afe7eb52cbe7c4e448cb5413245c547
ユーザ名 <hidebu09200202@gmail.com>
2018-06-20 15:39:25 +0900

    親子関係記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,16 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
   has_many :boards, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :follows, dependent: :destroy
   validates :email, presence: true
   validates :password, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
   def my_board?(board_user_id)
     id == board_user_id
+  end
+
+  def my_follow(board)
+    follows.find_by(user_id: id, board_id: board.id)
   end
 end
