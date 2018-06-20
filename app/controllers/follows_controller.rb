303ef6c97aaabab63b859ec4a4a6215b33725ab3
ユーザ名 <hidebu09200202@gmail.com>
2018-06-20 18:24:18 +0900

    before_action修正

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,4 +1,5 @@
 class FollowsController < ApplicationController
+  before_action :set_follow, only: %i[destroy]
 
   def create
     @follow = Follow.new(user_id: current_user.id, board_id: params[:board_id])
@@ -8,8 +9,13 @@ class FollowsController < ApplicationController
   end
 
   def destroy
-    @follow = Follow.find(params[:id])
     @follow.destroy
     redirect_to :back, danger: 'お気に入りを解除しました。'
+  end
+
+  private
+
+  def set_follow
+    @follow = Follow.find(params[:id])
   end
 end
