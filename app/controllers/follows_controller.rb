6faf40237d1b7dca9d60b132f2fa5e5045cdadb5
ユーザ名 <hidebu09200202@gmail.com>
2018-06-21 13:05:23 +0900

    indexアクション処理記述

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,6 +1,10 @@
 class FollowsController < ApplicationController
   before_action :set_follow, only: %i[destroy]
 
+  def index
+    @boards = Board.followed_boards(current_user).page(params[:page]).per(10)
+  end
+
   def create
     @follow = Follow.new(user_id: current_user.id, board_id: params[:board_id])
     if @follow.save
