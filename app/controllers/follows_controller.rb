1047da2f13e4b5fcb569fa4811c13031b2c2a4b1
ユーザ名 <hidebu09200202@gmail.com>
2018-06-25 15:56:28 +0900

    Ajax化記述

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -8,13 +8,22 @@ class FollowsController < ApplicationController
   def create
     @follow = Follow.new(user_id: current_user.id, board_id: params[:board_id])
     if @follow.save
-      redirect_to :back, success: 'お気に入りに登録しました。'
+      @board = @follow.board
+      respond_to do |format|
+      format.html { redirect_to :back }
+      format.js
+      end
     end
   end
 
   def destroy
-    @follow.destroy
-    redirect_to :back, danger: 'お気に入りを解除しました。'
+    if @follow.destroy
+      @board =  @follow.board
+      respond_to do |format|
+      format.html { redirect_to :back }
+      format.js
+      end
+    end
   end
 
   private
