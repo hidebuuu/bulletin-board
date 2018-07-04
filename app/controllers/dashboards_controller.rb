15602c6a393ba7f8c550227df78c0ef8eef3db51
ユーザ名 <hidebu09200202@gmail.com>
2018-07-04 11:52:56 +0900

    変数の定義変更

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,6 @@
 class DashboardsController < GeneralController
   def index
-    @notifications = Notification.notifications_by_condition(params[:condition])
-    @notifications = @notifications.created_after(current_user.last_logout_at).decorate
+    notifications = Notification.notifications_by_condition(params[:condition], current_user.id)
+    @notifications = notifications.created_after(current_user.last_logout_at).decorate
   end
 end
