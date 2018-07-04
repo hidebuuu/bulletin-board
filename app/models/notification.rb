b47e4952e8bf66a8b44b5b26556d21f7576faa14
ユーザ名 <hidebu09200202@gmail.com>
2018-07-04 16:50:21 +0900

    commentのお知らせ絞り込み条件変更

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,10 +1,10 @@
 class Notification < ApplicationRecord
   belongs_to :user
   belongs_to :target, polymorphic: true
-
   scope :board_search, ->(user_id) { where(target_type: 'Board').where.not(user_id: user_id) }
-  scope :comment_search, ->(user_id) { where(target_type: 'Comment').where.not(user_id: user_id) }
+  scope :comment_search, ->(board_ids) { where(target_type: 'Comment').where(target_id: Comment.where(board_id: board_ids).pluck(:id)) }
   scope :created_after, ->(time) { where('created_at > ?', time) }
+
 
   def comment?
     target.is_a?(Comment)
@@ -14,14 +14,14 @@ class Notification < ApplicationRecord
     target.is_a?(Board)
   end
 
-  def self.notifications_by_condition(condition, user_id)
+  def self.notifications_by_condition(condition, user_id, board_ids)
     case condition
     when 'board'
       Notification.board_search(user_id)
     when 'comment'
-      Notification.comment_search(user_id)
+      Notification.comment_search(board_ids)
     else
-      notifications = Notification.comment_search(user_id) + Notification.board_search(user_id)
+      notifications = Notification.comment_search(board_ids) + Notification.board_search(user_id)
       Notification.where(id: notifications.map(&:id))
     end
   end
