6173cb35263334c78f78fb7f9936db9290e505ca
ユーザ名 <hidebu09200202@gmail.com>
2018-07-04 19:01:59 +0900

    scopeを２分割

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,7 +2,8 @@ class Notification < ApplicationRecord
   belongs_to :user
   belongs_to :target, polymorphic: true
   scope :board_search, ->(user_id) { where(target_type: 'Board').where.not(user_id: user_id) }
-  scope :comment_search, ->(board_ids) { where(target_type: 'Comment').where(target_id: Comment.where(board_id: board_ids).pluck(:id)) }
+  scope :comment_search, ->{ where(target_type: 'Comment')}
+  scope :comment_board_search, ->(board_ids) { where(target_id: Comment.where(board_id: board_ids).pluck(:id)) }
   scope :created_after, ->(time) { where('created_at > ?', time) }
 
   def comment?
@@ -18,9 +19,9 @@ class Notification < ApplicationRecord
     when 'board'
       Notification.board_search(user_id)
     when 'comment'
-      Notification.comment_search(board_ids)
+      Notification.comment_search.comment_board_search(board_ids)
     else
-      notifications = Notification.comment_search(board_ids) + Notification.board_search(user_id)
+      notifications = Notification.comment_search.comment_board_search(board_ids) + Notification.board_search(user_id)
       Notification.where(id: notifications.map(&:id))
     end
   end
