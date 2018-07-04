07b961922d37e341ac2589f9782c32d0c6874556
ユーザ名 <hidebu09200202@gmail.com>
2018-07-04 11:55:51 +0900

    view側ではなく、データベース側でお知らせの出し分けをするよう変更

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,8 +2,8 @@ class Notification < ApplicationRecord
   belongs_to :user
   belongs_to :target, polymorphic: true
 
-  scope :board, -> { where(target_type: 'Board') }
-  scope :comment, -> { where(target_type: 'Comment') }
+  scope :board_search, ->(user_id) { where(target_type: 'Board').where.not(user_id: user_id) }
+  scope :comment_search, -> (user_id) { where(target_type: 'Comment').where.not(user_id: user_id) }
   scope :created_after, ->(time) { where('created_at > ?', time) }
 
   def comment?
@@ -13,15 +13,15 @@ class Notification < ApplicationRecord
   def board?
     target.is_a?(Board)
   end
-
-  def self.notifications_by_condition(condition)
+  def self.notifications_by_condition(condition, user_id)
     case condition
     when 'board'
-      Notification.board
+      Notification.board_search(user_id)
     when 'comment'
-      Notification.comment
+      Notification.comment_search(user_id)
     else
-      Notification.all
+      notifications = Notification.comment_search(user_id) + Notification.board_search(user_id)
+      Notification.where(id: notifications.map {|n| n.id } )
     end
   end
 end
