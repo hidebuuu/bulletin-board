92cdfcdb9641141dfa357e7154fd4707317e1c79
ユーザ名 <hidebu09200202@gmail.com>
2018-07-04 11:55:03 +0900

    三項演算子に変更

--- app/decorators/notification_decorator.rb
+++ app/decorators/notification_decorator.rb
@@ -2,46 +2,22 @@ class NotificationDecorator < ApplicationDecorator
   delegate_all
 
   def notification_image
-    if comment?
-      notification.target.board.image
-    elsif board?
-      notification.target.image
-    end
+    comment? ?  notification.target.board.image : notification.target.image
   end
 
   def notification_link
-    if comment?
-      h.board_path(notification.target.board)
-    elsif board?
-      h.board_path(notification.target)
-    end
+    comment? ? h.board_path(notification.target.board) : h.board_path(notification.target)
   end
 
   def notification_comment
-    if comment?
-      'あなたの掲示板にコメントがつきました'
-    elsif board?
-      '新しい掲示板が作成されました。'
-    end
+    comment? ? 'あなたの掲示板にコメントがつきました' : '新しい掲示板が作成されました。'
   end
 
   def title
-    if comment?
-      'コメント：'
-    elsif board?
-      'タイトル：'
-    end
+    comment? ? 'コメント：' : 'タイトル：'
   end
 
   def notification_info
-    if comment?
-      target.content
-    elsif board?
-      target.title
-    end
-  end
-
-  def should_notify?
-    notification.user != h.current_user
+    comment? ? target.content : target.title
   end
 end
