3e71c46095e71c2c9c58ff1868f3afc71909eb75
ユーザ名 <hidebu09200202@gmail.com>
2018-07-09 17:46:06 +0900

    enum設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,17 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :follows, dependent: :destroy
   validates :email, presence: true
-  validates :password, presence: true, confirmation: true
-  validates :password_confirmation, presence: true
+  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  enum user_type: {
+  standard_user: 0,
+  admin_user: 1
+}
+
   def my_board?(board_user_id)
     id == board_user_id
   end
