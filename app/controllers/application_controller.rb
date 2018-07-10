7fcd9142c06ddeb1878b943cdd51137a210b4ab7
ユーザ名 <hidebu09200202@gmail.com>
2018-07-10 18:50:03 +0900

    sider修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,13 +6,13 @@ class ApplicationController < ActionController::Base
   rescue_from ActionController::RoutingError, with: :render_404
   rescue_from Exception, with: :render_500
 
-  def render_404(e = nil)
-    logger.info "Rendering 404 with exception: #{ e.try(:message) || '' }"
-    render template: 'errors/error_404', status: :not_found,  content_type: 'text/html'
+  def render_404(error = nil)
+    logger.info "Rendering 404 with exception: #{error.try(:message) || ''}"
+    render template: 'errors/error_404', status: :not_found, content_type: 'text/html'
   end
 
-  def render_500(e = nil)
-    logger.info "Rendering 500 with exception: #{ e.try(:message) || '' }"
+  def render_500(error = nil)
+    logger.info "Rendering 500 with exception: #{error.try(:message) || ''}"
     render template: 'errors/error_500', status: :internal_server_error, content_type: 'text/html'
   end
 end
