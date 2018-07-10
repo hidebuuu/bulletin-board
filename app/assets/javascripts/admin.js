4bc54279fb6bdbdbd67719f543342c6287832627
ユーザ名 <hidebu09200202@gmail.com>
2018-07-10 11:53:59 +0900

    minファイル指定を通常ファイル指定へ。

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,3 +1,5 @@
+//= require jquery
+//= require jquery_ujs
 //= require jquery/dist/jquery.min
-//= require bootstrap/dist/js/bootstrap.min
+//= require bootstrap/dist/js/bootstrap
 //= require admin-lte/dist/js/adminlte.min
